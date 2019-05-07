b856a81757394522deb2e984fc80cdcb8cc8d84e
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-07 03:18:10 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -4,7 +4,8 @@ def levitation_quiz
 	loop do
 	  puts "What is the spell that enacts levitation?"
 	  answer=gets.chomp
-	  breal if answer== ""
+	  breal if answer== "Wingardium Leviosa"
 end
+puts ""
 
 
